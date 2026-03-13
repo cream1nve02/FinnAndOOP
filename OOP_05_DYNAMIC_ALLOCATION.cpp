c43a771992ddd1c8458624bf23174ba6df1b93d1
cream1nve02 <cream1nve@gmail.com>
2026-03-13 09:51:32 +0900

    DYNAMIC_ALLOCATION

---
+++ OOP_05_DYNAMIC_ALLOCATION.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int *a = new int(5);
+
+    cout << a << endl;
+    cout << *a << endl;
+    *a = 10;
+    cout << a << endl;
+    cout << *a << endl;
+
+    delete a;
+
+}
